f3e8238f0a5a8d12c83f5b954451096d628e2d5c
Heidi <111697947+heidimag@users.noreply.github.com>
2023-05-08 21:44:48 -0400

    PaymentProcess Class
    Not complete. Just the start of the class for the payment. The bill formatting will be added later.

---
+++ Final Project/src/FinalProject/PaymentProcess.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package electroshop1;
+
+import java.util.*;
+
+/**
+ *
+ * @author A
+ */
+
+public class PaymentProcess {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // initialize budget object with $1000
+        Budget budgetObject = new Budget(1000);
+
+        System.out.print("Enter the item price: $");
+        double price = scan.nextDouble();
+
+        if (price <= budgetObject.getBalance()) {
+            System.out.println("You have enough money to buy this item.");
+            System.out.print("Confirm transaction? (Y/N): ");
+            String confirm = scan.next();
+
+            if (confirm.equalsIgnoreCase("Y")) {
+                budgetObject.remove(price);
+                System.out.println("Transaction complete.");
+                System.out.print("Current balance: ");
+                budgetObject.displayBalance();
+            } else {
+                System.out.println("Transaction canceled.");
+            }
+        } else {
+            System.out.println("You do not have enough money to buy this item.");
+        }
+
+        scan.close();
+    }
+}
