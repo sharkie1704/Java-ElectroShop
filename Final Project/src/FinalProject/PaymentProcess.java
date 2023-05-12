4c3f3969b12168c1fd33b885ee82f78bd652f390
VStudent2000 <131478750+VStudent2000@users.noreply.github.com>
2023-05-12 11:54:26 -0400

    ntela

--- Final Project/src/FinalProject/PaymentProcess.java
+++ Final Project/src/FinalProject/PaymentProcess.java
@@ -16,6 +16,8 @@ import java.util.*;
  */
 public class PaymentProcess {
     public static void main(String[] args) {
+        System.out.println("Ntela was here");
+    }
 //        Scanner scan = new Scanner(System.in);
 
 //        // initialize budget object with $1000
