805cca8de6e8fd8fffd1a038cebe9446ec730859
VStudent2000 <131478750+VStudent2000@users.noreply.github.com>
2023-05-12 12:45:42 -0400

    Ntela (the filtering gives array + reader now made))
    bla bla

--- Final Project/src/FinalProject/Electronics.java
+++ Final Project/src/FinalProject/Electronics.java
@@ -10,105 +10,45 @@ import java.util.*;
  *
  * @author 2279307
  */
-public class Electronics {
+abstract public class Electronics {
 
     double price;
     String name;
     int id;
     int dateOfRelease;
 
-    public Electronics filtering(String s) {
+    public ArrayList<Electronics> filtering(ArrayList data,String s) {
         Electronics thing = null;
-        if ("a".equals(s)) { //phones
-            if (thing instanceof Phones) {
-                return thing;
-            }
-        }
+        ArrayList<Electronics> New = null;
+        String class_name;
 
-        if ("b".equals(s)) { //laptops
-            if (thing instanceof Laptops) {
-                return thing;
-            }
-        }
-
-        if ("c".equals(s)) { //tvs
-            if (thing instanceof TV) {
-                return thing;
-            }
-        }
-
-        if ("d".equals(s)) { //headphones
-            if (thing instanceof Headphones) {
-                return thing;
-            }
-        }
-        return null;
-    }
-
-    class Phones extends Electronics {
-
-        public Phones() {
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%12s: %-4s%-2s%d%n%12s: %d%n%12s: %f%c",
-                    "Product & id", name, "-", id,
-                    "Date", dateOfRelease, "Price", price, '$');
-        }
-    }
-
-    class Laptops extends Electronics {
-
-        @Override
-        public String toString() {
-            return String.format("%12s: %-4s%-2s%d%n%12s: %d%n%12s: %f%c",
-                    "Product & id", name, "-", id,
-                    "Date", dateOfRelease, "Price", price, '$');
-        }
-    }
-
-    class TV extends Electronics {
-
-        @Override
-        public String toString() {
-            return String.format("%12s: %-4s%-2s%d%n%12s: %d%n%12s: %f%c",
-                    "Product & id", name, "-", id,
-                    "Date", dateOfRelease, "Price", price, '$');
-        }
-    }
-
-    class Headphones extends Electronics {
-
-        @Override
-        public String toString() {
-            return String.format("%12s: %-4s%-2s%d%n%12s: %d%n%12s: %f%c",
-                    "Product & id", name, "-", id,
-                    "Date", dateOfRelease, "Price", price, '$');
-        }
-    }
-
-    class Customer {
-
-        String Addresse;
-        String Name;
-        ArrayList<Electronics> Cart;
-
-        public Customer(String Addresse, String Name) {
-            this.Addresse = Addresse;
-            this.Name = Name;
-        }
-
-        public static Electronics AddCart(ArrayList<Electronics> thing, int numb) {
-            for (int i = 0; i < thing.size(); i++) {
-                if (thing.get(i).getId() == numb) {
-                    return thing.get(i);
+        for(int i =0; i<data.size();i++){
+            switch(s){
+                case "a"->{
+                    if (data.get(i) instanceof Phones){
+                        New.add((Electronics) data.get(i));
+                    }
+                }
+                case "b"->{
+                    if (data.get(i) instanceof Laptops){
+                        New.add((Electronics) data.get(i));
+                    }
+                }
+                case "c"->{
+                    if (data.get(i) instanceof TV){
+                        New.add((Electronics) data.get(i));
+                    }
+                }
+                case "d"->{
+                    if (data.get(i) instanceof Headphones){
+                        New.add((Electronics) data.get(i));
+                    }
                 }
             }
-            return null;
         }
+        return New;
     }
-
+    
     public double getPrice() {
         return price;
     }
@@ -140,4 +80,99 @@ public class Electronics {
     public void setDateOfRelease(int dateOfRelease) {
         this.dateOfRelease = dateOfRelease;
     }
-}
+    
+    }
+    
+    class Phones extends Electronics {
+
+        public Phones(double p, String n, int i,int d) {
+            price = p;
+            name = n;
+            id = i;
+            dateOfRelease = d;
+        }
+        
+        @Override
+        public String toString() {
+            return String.format("%12s: %-4s%-2s%d%n%12s: %d%n%12s: %f%c",
+                    "Product & id", name, "-", id,
+                    "Date", dateOfRelease, "Price", price, '$');
+        }
+    }
+
+    class Laptops extends Electronics {
+
+        public Laptops(double p, String n, int i,int d) {
+            price = p;
+            name = n;
+            id = i;
+            dateOfRelease = d;
+        }
+        
+
+        @Override
+        public String toString() {
+            return String.format("%12s: %-4s%-2s%d%n%12s: %d%n%12s: %f%c",
+                    "Product & id", name, "-", id,
+                    "Date", dateOfRelease, "Price", price, '$');
+        }
+    }
+
+    class TV extends Electronics {
+
+        public TV(double p, String n, int i,int d) {
+            price = p;
+            name = n;
+            id = i;
+            dateOfRelease = d;
+        }
+        
+
+        @Override
+        public String toString() {
+            return String.format("%12s: %-4s%-2s%d%n%12s: %d%n%12s: %f%c",
+                    "Product & id", name, "-", id,
+                    "Date", dateOfRelease, "Price", price, '$');
+        }
+    }
+
+    class Headphones extends Electronics {
+
+        public Headphones(double p, String n, int i,int d) {
+            price = p;
+            name = n;
+            id = i;
+            dateOfRelease = d;
+        }
+        
+
+        @Override
+        public String toString() {
+            return String.format("%12s: %-4s%-2s%d%n%12s: %d%n%12s: %f%c",
+                    "Product & id", name, "-", id,
+                    "Date", dateOfRelease, "Price", price, '$');
+        }
+    }
+
+    class Customer {
+
+        String Addresse;
+        String Name;
+        ArrayList<Electronics> Cart;
+
+        public Customer(String Addresse, String Name) {
+            this.Addresse = Addresse;
+            this.Name = Name;
+        }
+
+        public static Electronics AddCart(ArrayList<Electronics> thing, int numb) {
+            for (int i = 0; i < thing.size(); i++) {
+                if (thing.get(i).getId() == numb) {
+                    return thing.get(i);
+                }
+            }
+            return null;
+        }
+    }
+
+
---
+++ Final Project/src/FinalProject/IO_Reader.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FinalProject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author 2280592
+ */
+
+
+public class IO_Reader {
+    public static void main(String[] args) throws IOException {
+        String fileName = "C:\\Users\\Ntela\\Desktop\\Note.txt";
+        String line = null;
+        List<String> object = new ArrayList<String>(5);
+        ArrayList products = new ArrayList();
+        String info = "";
+        try{
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            
+            while(null!= (line = bufferedReader.readLine())){
+             String[] data = line.split(" ");
+            
+              for(int i=0;data.length>i;i++){
+
+                for(int t = 0; t<data[i].length();t++){
+                    
+                    if(data[i].charAt(t)!='|'){
+                        info=info+data[i].charAt(t)+"";
+                    }
+                
+                if(data[i].charAt(t)=='|'){
+                    object.add(info);
+                    info ="";
+                }
+                
+                
+                if(object.size()==5){
+                switch (object.get(0)){
+                        case "p" -> {
+                            products.add(new Phones(Double.parseDouble(object.get(1)),object.get(2)
+                                    ,Integer.parseInt(object.get(3)),Integer.parseInt(object.get(4))));
+                            object.clear();
+                            }
+                            
+                        case "l" -> {
+                            products.add(new Laptops(Double.parseDouble(object.get(1)),object.get(2)
+                                    ,Integer.parseInt(object.get(3)),Integer.parseInt(object.get(4))));
+                            object.clear();
+                            }
+                        
+                        case "t" -> {
+                            products.add(new TV(Double.parseDouble(object.get(1)),object.get(2)
+                                    ,Integer.parseInt(object.get(3)),Integer.parseInt(object.get(4))));
+                            object.clear();
+                            }
+                        
+                        case "h" -> {
+                            products.add(new Headphones(Double.parseDouble(object.get(1)),object.get(2)
+                                    ,Integer.parseInt(object.get(3)),Integer.parseInt(object.get(4))));
+                            object.clear();
+                            }
+                            
+                        default -> object.clear();
+                    }
+                }
+                
+               }
+                
+            }
+              
+        }
+        bufferedReader.close();
+            
+        }
+        catch(FileNotFoundException ex){System.out.println("Unable to open file '"+fileName+"'");}
+        catch(IOException ex){System.out.println("Error reading file '"+fileName+"'");}
+        
+        
+    }
+}
