b4497a3b99c0a84589c6875902e84e1513a270af
Heidi <111697947+heidimag@users.noreply.github.com>
2023-05-13 21:56:09 -0400

    Update Admin.java
    Also added the modifyProduct mthd and started the removeProduct mtd (not done and barely started tbh).

--- Final Project/src/FinalProject/Admin.java
+++ Final Project/src/FinalProject/Admin.java
@@ -12,43 +12,41 @@ import java.util.Scanner;
  */
 public class Admin {
 
-    Scanner scan = new Scanner(System.in);
+    private Scanner input;
+
+    public Admin() {
+        input = new Scanner(System.in);
+    }
 
     public void displayOptions() {
-        System.out.println("Admin Menu");
+        System.out.println("Welcome, Admin. What would you like to do today?");
+        System.out.println("\nHere are some options: ");
         System.out.println("1. Add an item to the shop");
         System.out.println("2. Remove an item from the shop");
         System.out.println("3. Modify the attributes of an item");
         System.out.println("4. Log out");
 
-        // Check if there is input available before calling nextInt()
-        if (scan.hasNextInt()) {
-            int choice = scan.nextInt();
-
-            switch (choice) {
-                case 1 -> {
-                    // code to add an item to the shop
-                    addProduct();
-                    // chose a category + link with IO file
-                }
-                case 2 -> {
-                    // code to remove an item from the shop
-                    // chose a category + link with IO file
-                }
-                case 3 -> {
-                    // code to modify the attributes of an item
-                    // modify either price, date, etc.
-                }
-                case 4 -> // code to log out/ go to customer ???
-                    System.out.println("You have selected to log out.");
-                default ->
-                    System.out.println("Invalid choice. Please choose either options.");
-            }
-        } else {
-            System.out.println("Invalid input. Please enter a number.");
+        int choice = getChoice();
+        switch (choice) {
+            case 1:
+                addProduct();
+                break;
+            case 2:
+//                removeProduct();
+                break;
+            case 3:
+                modifyProduct();
+                break;
+            case 4:
+                System.out.println("Logging out...");
+                break;
+            default:
+                System.out.println("Invalid choice. Please choose a valid option.");
+                displayOptions();
         }
     }
-        private int getChoice() {
+
+    private int getChoice() {
         System.out.print("Please enter a number corresponding to an option: ");
         int choice = input.nextInt();
         input.nextLine(); //consume the newline character
@@ -101,5 +99,74 @@ public class Admin {
         }
 
     }
+    
+    // to remove a product 
+    public void removeItem() {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the ID of the item you want to remove: ");
+    int id = input.nextInt();
+    input.nextLine(); 
+
+    // search for the item with the given ID
+    /*
+     *  write the rest of the code here
+     */
+    
+}
+
+    // modify
+    public void modifyProduct() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the category to modify (p, h, t, or l):");
+    String category = scanner.nextLine();
+    System.out.println("Enter the name of the item to modify:");
+    String name = scanner.nextLine();
+    System.out.println("Enter the field to modify (name, price, ID, or date):");
+    String field = scanner.nextLine();
+    System.out.println("Enter the new value:");
+    String newValue = scanner.nextLine();
+
+    try {
+        File file = new File("C:\\Users\\2279307\\Desktop\\Note.txt");
+        File tempFile = new File("ModifiedProducts.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+        String currentLine;
+        while ((currentLine = reader.readLine()) != null) {
+            String[] itemData = currentLine.split("\\|");
+            if (itemData[0].equals(category) && itemData[2].equals(name)) {
+                switch (field) {
+                    case "name":
+                        itemData[2] = newValue;
+                        break;
+                    case "price":
+                        itemData[1] = newValue;
+                        break;
+                    case "ID":
+                        itemData[3] = newValue;
+                        break;
+                    case "date":
+                        itemData[4] = newValue;
+                        break;
+                    default:
+                        System.out.println("Invalid field entered");
+                        reader.close();
+                        writer.close();
+                        return;
+                }
+                currentLine = String.join("|", itemData);
+            }
+            writer.write(currentLine + "\n");
+        }
+        reader.close();
+        writer.close();
+        file.delete();
+        tempFile.renameTo(file);
+        System.out.println("Item modified successfully");
+    } catch (IOException e) {
+        System.out.println("Error modifying item: " + e.getMessage());
+    }
+}
 
 }
+
