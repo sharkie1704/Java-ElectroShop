ce0ba0dbc07ff79b931da64dab9ed1c2551d0dd6
sharkie1704 <130780201+sharkie1704@users.noreply.github.com>
2023-05-14 18:32:09 -0400

    Update EmptyCartException.java

--- Final Project/src/FinalProject/EmptyCartException.java
+++ Final Project/src/FinalProject/EmptyCartException.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package FinalProject;
 
-/**
- *
- * @author 2279307
- */
 public class EmptyCartException extends Exception {
 
     public EmptyCartException(String message) {
