f912ea33cc69b774412291c392612ac602db7e90
sharkie1704 <130780201+sharkie1704@users.noreply.github.com>
2023-05-14 18:29:51 -0400

    Update Electronics.java

--- Final Project/src/FinalProject/Electronics.java
+++ Final Project/src/FinalProject/Electronics.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package FinalProject;
 
 import java.util.*;
 
-/**
- *
- * @author 2279307
- */
 abstract public class Electronics {
 
     double price = 0.00;
