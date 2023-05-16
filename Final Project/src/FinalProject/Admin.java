82c255e6face63f5379c989d415deb98e43108f2
Heidi <111697947+heidimag@users.noreply.github.com>
2023-05-16 04:31:22 -0400

    Update Admin.java
    FIXED 100% WOKRING YESSS

--- Final Project/src/FinalProject/Admin.java
+++ Final Project/src/FinalProject/Admin.java
@@ -1,17 +1,17 @@
 package FinalProject;
 
-import java.io.*;
-import java.text.DecimalFormat;
-import java.util.*;
-
 public class Admin {
 
     /*
     ** The admin menu of options
      */
     public void displayOptions() {
-        System.out.printf("\n%s", "************************************************");
-        System.out.println("\nWelcome, Admin. What would you like to do today?");
+        System.out.println(
+                """
+              \n~~************************************************~~          
+                Welcome, Admin. What would you like to do today?
+              ~~************************************************~~
+                    """);
         System.out.print("""
                 1. Add an item to the shop
                 2. Remove an item from the shop
@@ -19,27 +19,57 @@ public class Admin {
                         """);
     }
 
-    /*
-    ** List of options that the admin can do
-     */
-    public void adminOptions(int adminChoice, Scanner input) throws IOException {
-        switch (adminChoice) {
-            case 1 ->
-                addProduct(input);
-            case 2 ->
-                removeProduct(input);
-            case 3 ->
-                System.out.printf("\n%20s\n", "Successfully logged out.");
-            default -> {
-                System.out.println("Invalid choice. Please choose a valid option.");
+/*
+** List of options that the admin can do
+*/
+public void adminOptions(int adminChoice, Scanner input) throws IOException {
+    boolean validChoice = false;
+
+    while (!validChoice) {
+        try {
+            switch (adminChoice) {
+                case 1 -> {
+                    addProduct(input);
+                    validChoice = true;
+                }
+                case 2 -> {
+                    removeProduct(input);
+                    validChoice = true;
+                }
+                case 3 -> {
+                System.out.println(
+                    """
+              ~~**********************************~~          
+                    Successfully logged out.
+              ~~**********************************~~ 
+                    """);
+                    validChoice = true;
+                }
+                default -> {
+                    System.out.println("Invalid choice. Please choose a valid option.");
+                    input.nextLine(); // Consume the invalid input
+                    System.out.print("Enter your choice: ");
+                    if (input.hasNextInt()) {
+                        adminChoice = input.nextInt();
+                    } else {
+                        throw new InputMismatchException();
+                    }
+                }
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid option.");
+            input.nextLine(); // Consume the invalid input
+            System.out.print("Enter your choice: ");
         }
     }
+}
+
 
     /*
     ** To add an item
      */
     public void addProduct(Scanner input) {
+        System.out.println("--------------------------------");
         System.out.println("Please choose a category:");
         System.out.println("p - phones");
         System.out.println("h - headphones");
@@ -69,11 +99,16 @@ public class Admin {
             String item = category + "|" + formattedPrice + "|" + name + "|" + id + "|" + year + "|";
 
             try {
-                FileWriter writer = new FileWriter("C:\\Users\\2235663\\Desktop\\Note.txt", true);
+                FileWriter writer = new FileWriter("C:\\Users\\A\\Desktop\\Note.txt", true);
                 try ( PrintWriter printWriter = new PrintWriter(writer)) {
                     printWriter.println(item);
                 }
-                System.out.println("Item added successfully.");
+                System.out.println(
+                        """
+              \n~~************************************************~~          
+                           Item successfully added.
+              ~~************************************************~~
+                    """);
             } catch (IOException e) {
                 System.out.println("An error occurred.");
             }
@@ -83,25 +118,31 @@ public class Admin {
     /*
     ** To remove an item
      */
-    public void removeProduct(Scanner input) throws IOException {
-        String fileName = "C:\\Users\\A\\Desktop\\Note.txt";
-        ArrayList<Electronics> electronicsList = new ArrayList();
-        IOReader.readElectronicsFile(fileName, electronicsList);
 
-        System.out.printf("\n%s\n", "************************************");
-        for (int i = 0; i < electronicsList.size(); i++) {
-            System.out.println("\n" + (i + 1) + ". " + electronicsList.get(i) + " ");
-        }
 
-        System.out.print("Enter the ID of the item you want to remove: ");
+public void removeProduct(Scanner input) throws IOException {
+    String fileName = "C:\\Users\\A\\Desktop\\Note.txt";
+    ArrayList<Electronics> electronicsList = new ArrayList();
+    IOReader.readElectronicsFile(fileName, electronicsList);
+    System.out.printf("\n%s\n", "---------------------------------------");
+    System.out.println("Here's the current inventory of items:");
+
+    for (int i = 0; i < electronicsList.size(); i++) {
+        System.out.println("\n" + (i + 1) + ". " + electronicsList.get(i) + " ");
+    }
+
+    boolean removed = false; // Flag indicating if the item was successfully removed
+
+    while (!removed) {
+        System.out.print("\nEnter the ID of the item you want to remove: ");
         int id = input.nextInt();
         input.nextLine(); //consume the newline character
 
         try {
-            File inputFile = new File("C:\\Users\\2235663\\Desktop\\Note.txt");
+            File inputFile = new File("C:\\Users\\A\\Desktop\\Note.txt");
             File tempFile = new File("temp.txt");
             BufferedWriter writer;
-            try ( BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
                 writer = new BufferedWriter(new FileWriter(tempFile));
                 String line;
                 while ((line = reader.readLine()) != null) {
@@ -109,6 +150,8 @@ public class Admin {
                     int itemID = Integer.parseInt(itemData[3]);
                     if (itemID != id) {
                         writer.write(line + "\n");
+                    } else {
+                        removed = true; // Item is found and removed
                     }
                 }
             }
@@ -120,9 +163,21 @@ public class Admin {
             if (!tempFile.renameTo(inputFile)) {
                 System.out.println("Could not rename file.");
             }
-            System.out.println("Item removed successfully.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
         }
+
+        if (removed) {
+            System.out.println(
+                    """
+              \n~~************************************************~~          
+                           Item successfully removed.
+              ~~************************************************~~
+                    """);
+        } else {
+            System.out.println("Item with ID " + id + " was not found. Please try again.");
+        }
     }
+}
+
 }
